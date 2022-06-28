1e907ce5fb58f80aae2e90aa49fb57e2019159d0
Abhishek Harish <itsjustabhii@gmail.com>
2022-06-28 11:15:54 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://itsjustabhii:Abhishek%401@nodejs-passport.wcc2y.mongodb.net/?retryWrites=true&w=majority'
+}
+
+/*New Password rules for MongDB - https://www.mongodb.com/docs/atlas/troubleshoot-connection/#special-characters-in-connection-string-password */
